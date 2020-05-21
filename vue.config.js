6176170872b62d3ed152127bf5056d5c5fe88c9f
Andy Rothwell <rothwell.andy@gmail.com>
2020-05-21 15:17:18 -0400

    fixes vue.config.js after merge from dev

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   filenameHashing: false,
-  publicPath: "/covid-testing/dev/",
+  publicPath: "/covid-testing-sites/",
+  // publicPath: "/covid-testing/dev/",
   chainWebpack: (config) => {
     config.resolve.symlinks(false);
   },
