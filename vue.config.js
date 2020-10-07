c1cedf75f5a47106c2b93ec29765609f306c1221
Andy Rothwell <rothwell.andy@gmail.com>
2020-10-07 12:50:02 -0400

    uses pinboard 0.5.3

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,30 @@
+const Visualizer = require('webpack-visualizer-plugin');
+
 module.exports = {
   filenameHashing: false,
   publicPath: "/covid-testing-sites/",
   //: "/covid-testing/dev/",
+  configureWebpack: {
+    plugins: [
+      new Visualizer({ filename: './statistics.html' }),
+    ],
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/](leaflet)[\\/]/,
+            name: 'leaflet-chunk',
+            chunks: 'all',
+          },
+          new: {
+            test: /[\\/]node_modules[\\/](esri-leaflet)[\\/]/,
+            name: 'esri-leaflet-chunk',
+            chunks: 'all',
+          },
+        },
+      },
+    },
+  },
   chainWebpack: (config) => {
     config.resolve.symlinks(false);
   },
