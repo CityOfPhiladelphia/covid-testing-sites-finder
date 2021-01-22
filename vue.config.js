b183b4db18ea9b7785424583611003d0800e4206
Andy Rothwell <rothwell.andy@gmail.com>
2021-01-22 13:58:16 -0500

    uses phila-ui branch of pinboard and removes foundation

--- vue.config.js
+++ vue.config.js
@@ -41,8 +41,15 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        prependData: `@import "@/scss/_variables.scss";
-              @import "@/scss/_mixins.scss";`,
+        prependData: `
+          @import "@/scss/_variables.scss";
+          @import "@/scss/_mixins.scss";
+        `,
+        data: `
+          @import "~@phila/phila-ui/src/assets/styles/scss/functions.scss";
+          @import "~@phila/phila-ui/src/assets/styles/scss/colors.scss";
+          @import "~@phila/phila-ui/src/assets/styles/scss/variables.scss";
+        `,
       },
     },
   },
