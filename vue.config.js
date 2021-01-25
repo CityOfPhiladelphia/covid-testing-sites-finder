4262e54c4e12db0061191fef0edbb81e69a369a1
Andy Rothwell <rothwell.andy@gmail.com>
2021-01-25 11:54:45 -0500

    testing vue.js.config stuff

--- vue.config.js
+++ vue.config.js
@@ -40,15 +40,12 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        prependData: `
-          @import "@/scss/_variables.scss";
-          @import "@/scss/_mixins.scss";
-        `,
         data: `
           @import "~@phila/phila-ui/src/assets/styles/scss/functions.scss";
           @import "~@phila/phila-ui/src/assets/styles/scss/colors.scss";
           @import "~@phila/phila-ui/src/assets/styles/scss/variables.scss";
         `,
+        sourceMap: true,
       },
     },
   },
