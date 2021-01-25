c470a1e7b47426f28287a9a2437427e77c43dfda
Andy Rothwell <rothwell.andy@gmail.com>
2021-01-25 14:44:50 -0500

    fixes imports of scss files

--- vue.config.js
+++ vue.config.js
@@ -29,18 +29,11 @@ module.exports = {
   },
   lintOnSave: true,
 
-  // css: {
-  //   loaderOptions: {
-  //     sass: {
-  //       data: '@import "@/scss/global.scss;',
-  //     },
-  //   },
-  // },
-
   css: {
     loaderOptions: {
       sass: {
         data: `
+          @import "~@phila/phila-ui/src/assets/styles/scss/all.scss";
           @import "~@phila/phila-ui/src/assets/styles/scss/functions.scss";
           @import "~@phila/phila-ui/src/assets/styles/scss/colors.scss";
           @import "~@phila/phila-ui/src/assets/styles/scss/variables.scss";
