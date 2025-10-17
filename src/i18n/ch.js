9577a3de29efd12dd6b1ed9a1ece48e3e1dda4f2
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2025-10-17 14:29:21 -0400

    finishes auto-translator setup

--- src/i18n/ch.js
+++ src/i18n/ch.js
@@ -1,139 +1,138 @@
-
-export default {
-  app: {
-    title: 'COVID-19 检测地点',
-    subtitle: '搜索您附近的 COVID-19 检测地点',
-    bannerAlert: '今天很多地点均关闭。有关更多信息，请查看具体地点详细信息。',
-    noResults: '在您的搜索范围内未找到检测地点。请致电您的医疗保健提供者或访问公共卫生部 (Department of Public Health) 的 COVID-19 网站，了解关于 <a href="https://www.phila.gov/programs/coronavirus-disease-2019-covid-19/guidance/faq/#getting-tested" target="_blank">在费城进行检测</a>的信息.',
+export default{
+  "app": {
+    "title": "COVID-19 测试地点",
+    "subtitle": "在你附近找一个 COVID-19 测试",
+    "bannerAlert": "今天许多网站都关闭了。查看特定的网站详细信息以获取更多信息。",
+    "noResults": "在您的搜索中未找到任何测试站点。请致电您的医疗保健提供者或访问公共卫生部的 COVID-19 网站，了解有关<a href='https://www.phila.gov/programs/coronavirus-disease-2019-covid-19/guidance/faq/#getting-tested' target='_blank'>在费城接受检测</a>的信息。"
   },
-  introPage: {
-    p1: '费城市有许多常驻及临时检测站。<a href="https://www.phila.gov/programs/coronavirus-disease-2019-covid-19/guidance/faq/#getting-tested" target="_blank">查看哪些人应接受检测。</a>',
-    section1Title: '查找临时检测服务站',
-    p2: '临时检测服务站设立在临时地点并且未在地图上显示。<a href="https://www.phila.gov/the-latest/all-events/?category=Mobile%20testing%20sites%20for%20COVID-19" target="_blank">查看检测服务的日程详情。</a>',
-    section2Title: '查找检测站',
-    p3: '使用此查找器，即可：',
-    ol1: {
-      li1: '定位费城市的 COVID-19 检测站。',
-      li2: '在地图上选择地点，即可了解详情。',
-      li3: '在去做检测之前，联系服务提供者。',
+  "introPage": {
+    "p1": "费城各地都有永久和临时的测试场所。<a href='https://www.phila.gov/programs/coronavirus-disease-2019-covid-19/guidance/faq/#getting-tested' target='_blank'>找出谁应该接受检测</a>。",
+    "section1Title": "查找弹出式测试活动",
+    "p2": "弹出式活动在临时地点举办，不显示在地图上。<a href='https://www.phila.gov/the-latest/all-events/?category=Mobile%20testing%20sites%20for%20COVID-19' target='_blank'>查看日历活动了解详情</a>。",
+    "section2Title": "查找测试地点",
+    "p3": "使用此查找器可以：",
+    "ol1": {
+      "li1": "在费城找一个 COVID-19 测试地点。",
+      "li2": "在地图上选择位置以了解详细信息。",
+      "li3": "在进行测试之前，请联系提供商。"
     },
-    p4: '检测站的提示和要求',
-    p5: '您不必自行承担检测费用。然而，一些检测站可能向您的保险计划开具账单以支付诊疗费。',
-    p6: '检测站可能要求您提供身份证件或健康保险信息。如果您没有上述证明，仍然可以进行检测。',
-    p7: '一些检测站可能：',
-    ul1: {
-      li1: '只对符合一定条件的人群提供检测。',
-      li2: '需要预约。',
-      li3: '要求您提供医生开具的转诊信函。',
-      li4: '要求您待在您的车里（如免下车检测服务站）。',
+    "p4": "在测试现场会发生什么",
+    "p5": "您无需自付费用即可参加考试。但是，某些网站可能会向您的保险收取访问费。",
+    "p6": "在现场，将要求您提供身份证明，还可能要求您提供健康保险信息。如果你没有这些，你仍然可以接受测试。",
+    "p7": "一些网站可能会：",
+    "ul1": {
+      "li1": "仅限符合特定标准的人进行测试。",
+      "li2": "需要预约。",
+      "li3": "需要医生的转诊。"
     },
-    p8: '您将被要求提供身份证明，还将要求您提供健康保险信息。需要身份证明才能接受测试。如果你没有健康保险，你仍然可以通过自付费用来接受检查。此外，某些网站可能需要预约。',
-    p9: '您将被要求提供身份证明，还将要求您提供健康保险信息。需要身份证明才能接受测试。如果你没有健康保险，你仍然可以通过自付费用来接受检查。',
-    callout1: {
-      p1: '<b>有疑问？</b> 联系您的医疗保健提供者或浏览网站 <a href="https://www.phila.gov/programs/coronavirus-disease-2019-covid-19/guidance/faq/#getting-tested" target="_blank">在费城参加检测</a>，查看常见问题与解答。',
-    },
+    "p8": "您将被要求提供身份证明，还将要求您提供健康保险信息。需要身份证明才能接受测试。如果你没有健康保险，你仍然可以通过自付费用来接受检查。此外，某些网站可能需要预约。",
+    "p9": "您将被要求提供身份证明，还将要求您提供健康保险信息。需要身份证明才能接受测试。如果你没有健康保险，你仍然可以通过自付费用来接受检查。",
+    "callout1": {
+      "p1": "<b>有问题吗？</b>致电您的医疗保健提供者或查看我们在<a href='https://www.phila.gov/programs/coronavirus-disease-2019-covid-19/guidance/faq/#getting-tested' target='_blank'>费城接受检测的</a>常见问题解答。"
+    }
   },
-  languages: {
-    languagesSpoken: '所用语言',
-    english: '英语',
-    spanish: '西班牙语',
-    chinese: '中文',
-    french: '法语',
-    arabic: '阿拉伯',
-    hindi: '印地语',
-    algerian: '阿尔及利亚人',
-    russian: '俄语',
-    translationServices: '电话译员服务：',
+  "languages": {
+    "languagesSpoken": "使用的语言",
+    "english": "英语",
+    "spanish": "西班牙的",
+    "chinese": "中国人",
+    "french": "法语",
+    "arabic": "阿拉伯语",
+    "hindi": "印地语",
+    "algerian": "阿尔及利亚的",
+    "russian": "俄国人",
+    "translationServices": "电话口译服务："
   },
-  address: 'chinese address',
-  keyword: 'chinese keyword',
-  beforeYouGo: '在您出发前请先了解以下信息',
-  checkSite: '查看特定场所的信息。 时间有可能会变动。',
-  hoursVary: '时间和可用性各不相同。',
-  eligibility: '详情',
-  testingHours: '检测时间',
-  daysOfOperation: '营业时间',
-  Monday: "星期一",
-  Tuesday: "星期二",
-  Wednesday: "星期三",
-  Thursday: "星期四",
-  Friday: "星期五",
-  Saturday: "星期六",
-  Sunday: "星期日",
-  // access: '访问',
-  Yes: '是',
-  No: '否',
-  Unknown: '未知',
-  website: '网站',
-  process: {
-    category: '流程',
-    driveThru: '免下车',
-    walkUp: '步行',
-    both: '免下车和步行',
+  "address": "地址",
+  "keyword": "关键字",
+  "beforeYouGo": "在你走之前",
+  "checkSite": "查看具体的站点信息。时间可能会发生变化。",
+  "hoursVary": "时间和可用性各不相同。",
+  "eligibility": "详情",
+  "testingHours": "测试时间",
+  "daysOfOperation": "运营天数",
+  "Monday": "星期一",
+  "Tuesday": "星期二",
+  "Wednesday": "周三",
+  "Thursday": "星期四",
+  "Friday": "星期五",
+  "Saturday": "星期六",
+  "Sunday": "星期日",
+  "Unknown": "未知",
+  "website": "网站",
+  "process": {
+    "category": "流程",
+    "driveThru": "Drive-thru",
+    "walkUp": "Walkup",
+    "both": "直通车和步行"
   },
-  symptomatic: {
-    category: '必须有症状',
-    null: '',
-    yes: '必须有症状',
-    no: '不需要症状（但可能需要暴露）',
+  "radio": {
+    "Yes": "是的",
+    "No": "没有"
   },
-  rapid: {
-    category: '快速测试',
-    Yes: '网站提供快速测试（请致电获取详细信息）',
-    No: '没有快速测试',
+  "symptomatic": {
+    "category": "必须有症状",
+    "null": "",
+    "yes": "必须有症状",
+    "no": "不需要出现症状（但可能需要接触）"
   },
-  pcr: {
-    category: '聚合酶链反应检测',
-    yes: '提供 PCR 检测（详情请致电）',
-    Yes: '提供 PCR 检测（详情请致电）',
-    no: '没有 PCR 检测',
-    No: '没有 PCR 检测',
+  "rapid": {
+    "category": "快速测试",
+    "Yes": "提供快速测试（详情请致电）",
+    "No": "没有快速测试"
   },
-  refReq: {
-    category: '需要转介',
-    null: '',
-    yes: '需要转介',
-    no: '不需要转介',
+  "pcr": {
+    "category": "聚合酶链反应测试",
+    "yes": "提供 PCR 测试（详情请致电）",
+    "Yes": "提供 PCR 测试（详情请致电）",
+    "no": "没有 PCR 测试",
+    "No": "没有 PCR 测试"
   },
-  patientAge: {
-    category: '患者年龄',
-    null: '',
-    year14: '14 岁以上',
-    year18: '18 岁以上',
-    pedCare: '提供儿科检测',
+  "refReq": {
+    "category": "需要推荐",
+    "null": "",
+    "yes": "需要推荐",
+    "no": "无需推荐"
   },
-  panelText: {
-    p1: '如果您无法通过您的医疗保健提供者进行 COVID-19 检测，此工具可以帮助您找到费城市内的免费检测地点。',
+  "patientAge": {
+    "category": "患者年龄",
+    "null": "",
+    "year14": "+14 岁",
+    "year18": "+18 岁",
+    "pedCare": "提供儿科检查"
   },
-  restrictions: {
-    lowInc: '此站点是为低收入家庭和个人准备的。',
-    year14: '必须 14 岁或以上。',
-    year18: '必须 18 岁或以上。',
-    netPat: '患者必须在提供者网络中才能在此站点接受检测。',
-    medPrior: '此站点将优先考虑医疗保健工作人员和急救人员。',
-    homeless: '此站点是为无家可归的人准备的。',
-    telemed: '在此站点进行检测前需要进行远程医疗看诊。',
-    onlineQuest: '来本站点之前必须完成在线调查问卷。',
+  "panelText": {
+    "p1": "如果您无法通过医疗保健提供者获得 COVID-19 检测，此工具可以帮助您在费城找到检测。"
   },
-  notes: {
-    schedApp: '检测需要预约。',
-    refReq: '需要转介。',
-    schedAppRefReq: '需要预约和转介。',
-    noApp: '检测无需预约。',
-    testAll: '即使未出现症状也可提供检测。',
+  "restrictions": {
+    "lowInc": "该网站适用于低收入家庭和个人。",
+    "year14": "必须年满 14 岁。",
+    "year18": "必须年满 18 岁。",
+    "netPat": "患者必须进入提供者的网络才能在此站点接受测试。",
+    "medPrior": "将优先考虑该站点的医护人员和急救人员。",
+    "homeless": "该网站专为无家可归者而设计。",
+    "telemed": "在此站点进行测试之前，需要进行远程医疗访问。",
+    "onlineQuest": "在访问本网站之前，必须填写在线问卷。"
   },
-  facilityType: {
-    drugstore: '药房',
-    fieldSite: '户外地点',
-    clinic: '诊所',
-    hospital: '医院',
-    other: '其他',
-    phmcHC: 'PHMC 健康中心',
-    urgentCare: '急救中心',
-    cityHC: '市健康中心',
-    homelessServices: '无家可归者服务',
+  "notes": {
+    "schedApp": "需要预约才能进行测试。",
+    "refReq": "需要推荐。",
+    "schedAppRefReq": "需要预约和推荐。",
+    "noApp": "无需预约即可进行测试。",
+    "testAll": "即使没有症状也能进行检测。"
   },
-  tooltips: {
-    symptomatic: '如需了解更多信息，请查阅冠状病毒病的症状 (CDC)',
+  "facilityType": {
+    "drugstore": "药店",
+    "fieldSite": "实地现场",
+    "clinic": "诊所",
+    "hospital": "医院",
+    "other": "其他",
+    "phmcHC": "PHMC 健康中心",
+    "urgentCare": "紧急护理",
+    "cityHC": "城市卫生中心",
+    "homelessServices": "无家可归服务"
   },
-};
+  "tooltips": {
+    "symptomatic": "有关更多信息，请参阅冠状病毒症状（CDC）"
+  }
+}
--- src/i18n/vi.js
+++ src/i18n/vi.js
@@ -1,139 +1,138 @@
-
-export default {
-  app: {
-    title: 'Các cơ sở xét nghiệm COVID-19',
-    subtitle: 'Tìm cơ sở xét nghiệm COVID-19 gần quý vị',
-    bannerAlert: 'Hôm nay, nhiều địa điểm đóng cửa. Hãy kiểm tra các chi tiết về địa điểm cụ thể để biết thêm thông tin.',
-    noResults: 'Không tìm thấy cơ sở xét nghiệm trong phạm vi tìm kiếm của quý vị. Vui lòng gọi điện cho nhà cung cấp dịch vụ chăm sóc sức khỏe của quý vị hoặc truy cập trang web về COVID-19 của Sở Y Tế Công Cộng để biết thông tin về a href="https://www.phila.gov/programs/coronavirus-disease-2019-covid-19/guidance/faq/#getting-tested" target="_blank">cách nhận xét nghiệm tại Philadelphia</a>.',
+export default{
+  "app": {
+    "title": "Địa điểm thử nghiệm COVID-19",
+    "subtitle": "Tìm xét nghiệm COVID-19 gần bạn",
+    "bannerAlert": "Nhiều trang web đã đóng cửa ngày hôm nay. Kiểm tra chi tiết trang web cụ thể để biết thêm thông tin.",
+    "noResults": "Không tìm thấy trang web thử nghiệm nào trong tìm kiếm của bạn. Vui lòng gọi cho nhà cung cấp dịch vụ chăm sóc sức khỏe của bạn hoặc truy cập trang web COVID-19 của Bộ Y tế Công cộng để biết thông tin về <a href='https://www.phila.gov/programs/coronavirus-disease-2019-covid-19/guidance/faq/#getting-tested' target='_blank'>việc làm xét nghiệm ở Philadelphia</a>."
   },
-  introPage: {
-    p1: 'Có các địa điểm xét nghiệm cố định và tạm thời trên khắp Philadelphia. <a href="https://www.phila.gov/programs/coronavirus-disease-2019-covid-19/guidance/faq/#getting-tested" target="_blank">Tìm hiểu xem ai nên xét nghiệm</a>.',
-    section1Title: 'Tìm một sự kiện xét nghiệm lưu động',
-    p2: 'Sự kiện lưu động được tổ chức ở các địa điểm tạm thời và không được hiển thị trên bản đồ. <a href="https://www.phila.gov/the-latest/all-events/?category=Mobile%20testing%20sites%20for%20COVID-19" target="_blank">Kiểm tra sự kiện trên lịch để biết thông tin chi tiết</a>.',
-    section2Title: 'Tìm một địa điểm xét nghiệm',
-    p3: 'Sử dụng trình tìm kiếm này để:',
-    ol1: {
-      li1: 'Định vị một địa điểm xét nghiệm COVID-19 ở Philadelphia.',
-      li2: 'Chọn địa điểm trên bản đồ để biết thông tin chi tiết.',
-      li3: 'Liên hệ với nhà cung cấp trước khi đi xét nghiệm.',
+  "introPage": {
+    "p1": "Có các địa điểm thử nghiệm vĩnh viễn và tạm thời trên khắp Philadelphia. <a href='https://www.phila.gov/programs/coronavirus-disease-2019-covid-19/guidance/faq/#getting-tested' target='_blank'>Tìm ra ai nên được kiểm tra</a>.",
+    "section1Title": "Tìm sự kiện thử nghiệm bật lên",
+    "p2": "Các sự kiện bật lên được lưu trữ tại các địa điểm tạm thời và không được hiển thị trên bản đồ. <a href='https://www.phila.gov/the-latest/all-events/?category=Mobile%20testing%20sites%20for%20COVID-19' target='_blank'>Kiểm tra sự kiện lịch để biết chi tiết</a>.",
+    "section2Title": "Tìm một trang web thử nghiệm",
+    "p3": "Sử dụng công cụ tìm kiếm này để:",
+    "ol1": {
+      "li1": "Xác định vị trí địa điểm xét nghiệm COVID-19 ở Philadelphia.",
+      "li2": "Chọn vị trí trên bản đồ để biết chi tiết.",
+      "li3": "Liên hệ với nhà cung cấp trước khi đi kiểm tra."
     },
-    p4: 'Những điều nên dự kiến ở địa điểm xét nghiệm',
-    p5: 'Bạn sẽ không phải trả tiền túi để làm xét nghiệm. Tuy nhiên, một số địa điểm có thể lập hóa đơn bảo hiểm của bạn đối với phí thăm khám.',
-    p6: 'Ở địa điểm, bạn sẽ được yêu cầu trình giấy tờ nhận dạng và cũng có thể được yêu cầu cung cấp thông tin bảo hiểm y tế. Nếu không có những thứ này, bạn vẫn có thể làm xét nghiệm.',
-    p7: 'Một số địa điểm có thể:',
-    ul1: {
-      li1: 'Hạn chế xét nghiệm đối với những người đáp ứng các tiêu chí nhất định.',
-      li2: 'Yêu cầu hẹn trước.',
-      li3: 'Yêu cầu có giới thiệu từ bác sĩ của bạn.',
-      li4: 'Yêu cầu bạn ngồi trong xe (đối với các địa điểm cho phép lái xe qua).',
+    "p4": "Những gì mong đợi tại một địa điểm thử nghiệm",
+    "p5": "Bạn sẽ không phải trả tiền túi để làm bài kiểm tra. Tuy nhiên, một số trang web có thể tính phí bảo hiểm của bạn cho một khoản phí tham quan.",
+    "p6": "Tại trang web, bạn sẽ được yêu cầu nhận dạng và cũng có thể được yêu cầu cung cấp thông tin bảo hiểm y tế. Nếu bạn không có những thứ này, bạn vẫn có thể làm bài kiểm tra.",
+    "p7": "Một số trang web có thể:",
+    "ul1": {
+      "li1": "Giới hạn thử nghiệm cho những người đáp ứng các tiêu chí nhất định.",
+      "li2": "Yêu cầu một cuộc hẹn.",
+      "li3": "Yêu cầu sự giới thiệu từ bác sĩ của bạn."
     },
-    p8: 'Bạn sẽ được yêu cầu nhận dạng và cũng sẽ được yêu cầu cung cấp thông tin bảo hiểm y tế. Giấy tờ tùy thân sẽ được yêu cầu để nhận được một bài kiểm tra. Nếu bạn không có bảo hiểm y tế, bạn vẫn có thể nhận được một bài kiểm tra bằng cách trả tiền tự trả tiền. Ngoài ra, một số trang web có thể yêu cầu một cuộc hẹn.',
-    p9: 'Bạn sẽ được yêu cầu nhận dạng và cũng sẽ được yêu cầu cung cấp thông tin bảo hiểm y tế. Giấy tờ tùy thân sẽ được yêu cầu để nhận được một bài kiểm tra. Nếu bạn không có bảo hiểm y tế, bạn vẫn có thể nhận được một bài kiểm tra bằng cách trả tiền tự trả tiền.',
-    callout1: {
-      p1: '<b>Có thắc mắc?</b> Gọi cho nhà cung cấp dịch vụ chăm sóc sức khỏe của bạn hoặc xem Các Câu hỏi Thường gặp của chúng tôi về <a href="https://www.phila.gov/programs/coronavirus-disease-2019-covid-19/guidance/faq/#getting-tested" target="_blank">đi xét nghiệm ở Philadelphia</a>.',
-    },
+    "p8": "Bạn sẽ được yêu cầu nhận dạng và cũng sẽ được yêu cầu cung cấp thông tin bảo hiểm y tế. Giấy tờ tùy thân sẽ được yêu cầu để nhận được một bài kiểm tra. Nếu bạn không có bảo hiểm y tế, bạn vẫn có thể nhận được một bài kiểm tra bằng cách trả tiền tự trả tiền. Ngoài ra, một số trang web có thể yêu cầu một cuộc hẹn.",
+    "p9": "Bạn sẽ được yêu cầu nhận dạng và cũng sẽ được yêu cầu cung cấp thông tin bảo hiểm y tế. Giấy tờ tùy thân sẽ được yêu cầu để nhận được một bài kiểm tra. Nếu bạn không có bảo hiểm y tế, bạn vẫn có thể nhận được một bài kiểm tra bằng cách trả tiền tự trả tiền.",
+    "callout1": {
+      "p1": "<b>Câu hỏi?</b> Gọi cho nhà cung cấp dịch vụ chăm sóc sức khỏe của bạn hoặc xem Câu hỏi thường gặp của chúng <a href='https://www.phila.gov/programs/coronavirus-disease-2019-covid-19/guidance/faq/#getting-tested' target='_blank'>tôi về việc đi xét nghiệm ở Philadelphia</a>."
+    }
   },
-  languages: {
-    languagesSpoken: 'Các ngôn ngữ được nói',
-    english: 'Tiếng Anh',
-    spanish: 'Tiếng Tây Ban Nha',
-    chinese: 'Tiếng Trung Quốc',
-    french: 'Tiếng Pháp',
-    arabic: 'Tiếng Ả Rập',
-    hindi: 'Tiếng Hindi',
-    algerian: 'Người Algeria',
-    russian: 'Tiếng Nga',
-    translationServices: 'Dịch vụ thông dịch qua điện thoại:',
+  "languages": {
+    "languagesSpoken": "Ngôn ngữ được sử dụng",
+    "english": "Anh",
+    "spanish": "người Tây Ban Nha",
+    "chinese": "Trung Quốc",
+    "french": "người Pháp",
+    "arabic": "tiếng Ả Rập",
+    "hindi": "Tiếng Hindi",
+    "algerian": "Người An-giê-ri",
+    "russian": "Tiếng nga",
+    "translationServices": "Dịch vụ phiên dịch qua điện thoại:"
   },
-  address: 'vietnamese address',
-  keyword: 'vietnamese keyword',
-  beforeYouGo: 'Trước khi quý vị đến',
-  checkSite: 'Kiểm tra thông tin địa điểm cụ thể. Giờ hoạt động có thể thay đổi.',
-  hoursVary: 'Giờ làm việc và tính sẵn có khác nhau.',
-  eligibility: 'Chi tiết',
-  testingHours: 'Giờ xét nghiệm',
-  daysOfOperation: 'Ngày làm việc',
-  Monday: "Thứ Hai",
-  Tuesday: "Thứ Ba",
-  Wednesday: "Thứ Tư",
-  Thursday: "Thứ Năm",
-  Friday: "Thứ Sáu",
-  Saturday: "Thứ Bảy",
-  Sunday: "Chủ nhật",
-  // access: 'Truy cập',
-  Yes: 'Có',
-  No: 'Không',
-  Unknown: 'Không biết',
-  website: 'Trang web',
-  process: {
-    category: 'Quy trình',
-    driveThru: 'Lái xe qua',
-    walkUp: 'Đi bộ vào',
-    both: 'Lái xe qua & đi bộ vào',
+  "address": "địa chỉ",
+  "keyword": "từ khóa",
+  "beforeYouGo": "Trước khi bạn đi",
+  "checkSite": "Kiểm tra thông tin trang web cụ thể. Giờ có thể thay đổi.",
+  "hoursVary": "Giờ và tình trạng sẵn có khác nhau.",
+  "eligibility": "Chi tiết",
+  "testingHours": "Giờ thử nghiệm",
+  "daysOfOperation": "Ngày hoạt động",
+  "Monday": "Thứ hai",
+  "Tuesday": "Thứ ba",
+  "Wednesday": "Thứ tư",
+  "Thursday": "Thứ năm",
+  "Friday": "Thứ sáu",
+  "Saturday": "ngày thứ bảy",
+  "Sunday": "chủ nhật",
+  "Unknown": "Không xác định",
+  "website": "Trang web",
+  "process": {
+    "category": "Quy trình",
+    "driveThru": "Drive-thru",
+    "walkUp": "Đi bộ lên",
+    "both": "Lái xe và đi bộ"
   },
-  symptomatic: {
-    category: 'Phải có triệu chứng',
-    null: '',
-    yes: 'Phải có triệu chứng',
-    no: 'Các triệu chứng không bắt buộc (nhưng có thể bị phơi nhiễm)',
+  "radio": {
+    "Yes": "Vâng",
+    "No": "Không"
   },
-  rapid: {
-    category: 'Kiểm tra nhanh',
-    Yes: 'Cung cấp thử nghiệm nhanh (gọi để biết chi tiết)',
-    No: 'Không có thử nghiệm nhanh',
+  "symptomatic": {
+    "category": "Phải có triệu chứng",
+    "null": "",
+    "yes": "Phải có triệu chứng",
+    "no": "Các triệu chứng không bắt buộc (nhưng có thể tiếp xúc)"
   },
-  pcr: {
-    category: 'Xét nghiệm PCR',
-    yes: 'Cung cấp xét nghiệm PCR (gọi để biết chi tiết)',
-    Yes: 'Cung cấp xét nghiệm PCR (gọi để biết chi tiết)',
-    no: 'Không có xét nghiệm PCR',
-    No: 'Không có xét nghiệm PCR',
+  "rapid": {
+    "category": "Kiểm tra nhanh",
+    "Yes": "Cung cấp thử nghiệm nhanh (gọi để biết chi tiết)",
+    "No": "Không kiểm tra nhanh"
   },
-  refReq: {
-    category: 'Phải có giấy giới thiệu',
-    null: '',
-    yes: 'Phải có giấy giới thiệu',
-    no: 'Không yêu cầu có giấy giới thiệu',
+  "pcr": {
+    "category": "Xét nghiệm PCR",
+    "yes": "Cung cấp xét nghiệm PCR (gọi để biết chi tiết)",
+    "Yes": "Cung cấp xét nghiệm PCR (gọi để biết chi tiết)",
+    "no": "Không xét nghiệm PCR",
+    "No": "Không xét nghiệm PCR"
   },
-  patientAge: {
-    category: 'Tuổi của bệnh nhân',
-    null: '',
-    year14: 'Trên 14 tuổi',
-    year18: 'Trên 18 tuổi',
-    pedCare: 'Cung cấp dịch vụ xét nghiệm nhi khoa',
+  "refReq": {
+    "category": "Yêu cầu giới thiệu",
+    "null": "",
+    "yes": "Yêu cầu giới thiệu",
+    "no": "Không cần giới thiệu"
   },
-  panelText: {
-    p1: 'Nếu quý vị không được xét nghiệm COVID-19 thông qua nhà cung cấp dịch vụ chăm sóc sức khỏe của quý vị, thì công cụ này có thể giúp quý vị tìm cơ sở xét nghiệm miễn phí trong phạm vi Thành Phố Philadelphia.',
+  "patientAge": {
+    "category": "Tuổi bệnh nhân",
+    "null": "",
+    "year14": "+14 tuổi",
+    "year18": "+18 tuổi",
+    "pedCare": "Cung cấp xét nghiệm nhi khoa"
   },
-  restrictions: {
-    lowInc: 'Cơ sở này dành cho các gia đình và cá nhân có thu nhập thấp.',
-    year14: 'Phải từ 14 tuổi trở lên.',
-    year18: 'Phải từ 18 tuổi trở lên.',
-    netPat: 'Bệnh nhân phải thuộc mạng lưới của nhà cung cấp để được xét nghiệm tại cơ sở này.',
-    medPrior: 'Ưu tiên cho các nhân viên chăm sóc sức khỏe và nhân viên phản ứng tuyến đầu tại cơ sở này.',
-    homeless: 'Cơ sở này dành cho người vô gia cư.',
-    telemed: 'Phải được thăm khám y tế qua điện thoại trước khi xét nghiệm tại cơ sở này.',
-    onlineQuest: 'Phải hoàn tất bảng câu hỏi trực tuyến trước khi đến thăm khám tại cơ sở này.',
+  "panelText": {
+    "p1": "Nếu bạn không thể làm xét nghiệm COVID-19 thông qua nhà cung cấp dịch vụ chăm sóc sức khỏe của mình, công cụ này có thể giúp bạn tìm xét nghiệm trong Thành phố Philadelphia."
   },
-  notes: {
-    schedApp: 'Yêu cầu đặt lịch hẹn để xét nghiệm',
-    refReq: 'Phải có giấy giới thiệu.',
-    schedAppRefReq: 'Phải có lịch hẹn và giấy giới thiệu.',
-    noApp: 'Không cần đặt lịch hẹn để xét nghiệm.',
-    testAll: 'Thực hiện xét nghiệm ngay cả khi không có triệu chứng.',
+  "restrictions": {
+    "lowInc": "Trang web này dành cho các gia đình và cá nhân có thu nhập thấp.",
+    "year14": "Phải từ 14 tuổi trở lên.",
+    "year18": "Phải từ 18 tuổi trở lên.",
+    "netPat": "Một bệnh nhân phải ở trong mạng lưới của nhà cung cấp để nhận xét nghiệm tại trang web này.",
+    "medPrior": "Ưu tiên sẽ được trao cho nhân viên chăm sóc sức khỏe và nhân viên ứng phó đầu tiên tại trang web này.",
+    "homeless": "Trang web này dành cho những người trải qua tình trạng vô gia cư.",
+    "telemed": "Cần phải thăm khám y tế từ xa trước khi thử nghiệm tại trang web này.",
+    "onlineQuest": "Một bảng câu hỏi trực tuyến phải được hoàn thành trước khi truy cập trang web này."
   },
-  facilityType: {
-    drugstore: 'Hiệu Thuốc',
-    fieldSite: 'Cơ Sở Tại Hiện Trường',
-    clinic: 'Phòng Khám',
-    hospital: 'Bệnh Viện',
-    other: 'Khác',
-    phmcHC: 'Trung tâm y tế PHMC',
-    urgentCare: 'Chăm Sóc Khẩn Cấp',
-    cityHC: 'Trung tâm y tế thành phố',
-    homelessServices: 'Dịch vụ vô gia cư',
+  "notes": {
+    "schedApp": "Các cuộc hẹn là bắt buộc để kiểm tra.",
+    "refReq": "Yêu cầu giới thiệu.",
+    "schedAppRefReq": "Yêu cầu hẹn và giới thiệu.",
+    "noApp": "Không có cuộc hẹn cần thiết để kiểm tra.",
+    "testAll": "Xét nghiệm được cung cấp ngay cả khi các triệu chứng không có."
   },
-  tooltips: {
-    symptomatic: 'Để biết thêm thông tin, hãy xem Các triệu chứng bệnh khi nhiễm vi-rút corona (CDC)',
+  "facilityType": {
+    "drugstore": "Nhà thuốc",
+    "fieldSite": "Trang web hiện trường",
+    "clinic": "Phòng khám",
+    "hospital": "Bệnh viện",
+    "other": "Khác",
+    "phmcHC": "Trung tâm y tế PHMC",
+    "urgentCare": "Chăm sóc khẩn cấp",
+    "cityHC": "Trung tâm y tế thành phố",
+    "homelessServices": "Dịch vụ vô gia cư"
   },
-};
+  "tooltips": {
+    "symptomatic": "Để biết thêm thông tin, hãy xem Triệu chứng của coronavirus (CDC)"
+  }
+}
