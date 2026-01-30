52849f6d02f5ab6a19b6c19a0c6842eed5841591
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2026-01-30 15:10:46 -0500

    adds 3 new languages

---
+++ src/i18n/ht.js
@@ -0,0 +1,138 @@
+export default{
+  "app": {
+    "title": "Sit tès COVID-19",
+    "subtitle": "Jwenn yon tès COVID-19 tou pre ou",
+    "bannerAlert": "Anpil sit yo fèmen jodi a. Tcheke detay sit espesifik pou plis enfòmasyon.",
+    "noResults": "Pa te jwenn okenn sit tès nan rechèch ou. Tanpri rele founisè swen sante ou oswa vizite sit entènèt COVID-19 Depatman Sante Piblik la pou enfòmasyon sou <a href='https://www.phila.gov/programs/coronavirus-disease-2019-covid-19/guidance/faq/#getting-tested' target='_blank'>jwenn teste nan Philadelphia</a>."
+  },
+  "introPage": {
+    "p1": "Gen sit tès pèmanan ak tanporè nan tout Philadelphia. <a href='https://www.phila.gov/programs/coronavirus-disease-2019-covid-19/guidance/faq/#getting-tested' target='_blank'>Chèche konnen ki moun ki ta dwe jwenn teste</a>.",
+    "section1Title": "Jwenn yon evènman tès pop-up",
+    "p2": "Evènman pop-up yo anime nan kote tanporè epi yo pa montre sou kat jeyografik la. <a href='https://www.phila.gov/the-latest/all-events/?category=Mobile%20testing%20sites%20for%20COVID-19' target='_blank'>Tcheke evènman kalandriye a pou det</a> ay.",
+    "section2Title": "Jwenn yon sit tès",
+    "p3": "Sèvi ak chèche sa a pou:",
+    "ol1": {
+      "li1": "Lokalize yon sit tès COVID-19 nan Philadelphia.",
+      "li2": "Chwazi kote a sou kat jeyografik la pou detay yo.",
+      "li3": "Kontakte founisè a anvan ou ale pou yon tès."
+    },
+    "p4": "Ki sa ki atann nan yon sit tès",
+    "p5": "Ou pa pral oblije peye soti-of-pòch pou jwenn yon tès. Sepandan, gen kèk sit pouvwa bòdwo asirans ou pou yon frè vizit.",
+    "p6": "Nan sit la, yo pral mande ou pou idantifikasyon epi yo ka mande tou pou enfòmasyon asirans sante. Si ou pa gen sa yo, ou ka toujou jwenn yon tès.",
+    "p7": "Gen kèk sit pouvwa:",
+    "ul1": {
+      "li1": "Limite tès pou moun ki satisfè sèten kritè.",
+      "li2": "Mande pou yon randevou.",
+      "li3": "Mande pou yon referans nan men doktè ou."
+    },
+    "p8": "Yo pral mande ou pou idantifikasyon epi yo pral mande tou pou enfòmasyon asirans sante. Idantifikasyon yo pral oblije resevwa yon tès. Si ou pa gen asirans sante, ou ka toujou kapab resevwa yon tès pa peye soti-of-pòch. Anplis de sa, kèk sit ka mande pou yon randevou.",
+    "p9": "Yo pral mande ou pou idantifikasyon epi yo pral mande tou pou enfòmasyon asirans sante. Idantifikasyon yo pral oblije resevwa yon tès. Si ou pa gen asirans sante, ou ka toujou kapab resevwa yon tès pa peye soti-of-pòch.",
+    "callout1": {
+      "p1": "<b>Kesyon?</b> Rele founisè swen sante ou oswa wè FAQ nou an sou ap <a href='https://www.phila.gov/programs/coronavirus-disease-2019-covid-19/guidance/faq/#getting-tested' target='_blank'>resevwa teste nan Philadelphia</a>."
+    }
+  },
+  "languages": {
+    "languagesSpoken": "Lang pale",
+    "english": "Anglè",
+    "spanish": "Panyòl",
+    "chinese": "Chinwa",
+    "french": "franse",
+    "arabic": "Arab",
+    "hindi": "Hindi",
+    "algerian": "Aljeryen",
+    "russian": "Ris",
+    "translationServices": "Sèvis entèpretasyon telefòn:"
+  },
+  "address": "adrès",
+  "keyword": "mo kle",
+  "beforeYouGo": "Anvan ou ale",
+  "checkSite": "Tcheke enfòmasyon sou sit espesifik la. Èdtan yo sijè a chanjman.",
+  "hoursVary": "Èdtan ak disponiblite varye.",
+  "eligibility": "Detay",
+  "testingHours": "Èdtan tès",
+  "daysOfOperation": "Jou operasyon",
+  "Monday": "Lendi",
+  "Tuesday": "Madi",
+  "Wednesday": "Mèkredi",
+  "Thursday": "Jedi",
+  "Friday": "Vandredi",
+  "Saturday": "Samdi",
+  "Sunday": "Dimanch",
+  "Unknown": "Enkoni",
+  "website": "sit wèb",
+  "process": {
+    "category": "Pwosesis",
+    "driveThru": "Drive-Through",
+    "walkUp": "Walk-up",
+    "both": "Drive-through & Walk-up"
+  },
+  "radio": {
+    "Yes": "Wi",
+    "No": "Non"
+  },
+  "symptomatic": {
+    "category": "Dwe dwe sentomatik",
+    "null": "",
+    "yes": "Dwe dwe sentomatik",
+    "no": "Sentòm yo pa obligatwa (men ekspoze ta ka)"
+  },
+  "rapid": {
+    "category": "Rapid tès",
+    "Yes": "Ofri tès rapid (rele pou detay)",
+    "No": "Pa gen tès rapid"
+  },
+  "pcr": {
+    "category": "PCR tès",
+    "yes": "Ofri tès PCR (rele pou detay)",
+    "Yes": "Ofri tès PCR (rele pou detay)",
+    "no": "Pa gen tès PCR",
+    "No": "Pa gen tès PCR"
+  },
+  "refReq": {
+    "category": "Referans obligatwa",
+    "null": "",
+    "yes": "Referans obligatwa",
+    "no": "Pa gen referans obligatwa"
+  },
+  "patientAge": {
+    "category": "Laj pasyan",
+    "null": "",
+    "year14": "+14 ane fin vye granmoun",
+    "year18": "+18 zan",
+    "pedCare": "Ofri tès pedyatrik"
+  },
+  "panelText": {
+    "p1": "Si ou pa kapab jwenn yon tès COVID-19 atravè founisè swen sante ou, zouti sa a ka ede w jwenn yon tès nan vil Philadelphia."
+  },
+  "restrictions": {
+    "lowInc": "Se sit sa a gen entansyon pou fanmi ki ba-revni ak moun.",
+    "year14": "Dwe gen 14 ane oswa pi gran.",
+    "year18": "Dwe 18 ane oswa pi gran.",
+    "netPat": "Yon pasyan dwe nan rezo founisè a pou resevwa yon tès nan sit sa a.",
+    "medPrior": "Pral bay priyorite travayè swen sante ak premye moun ki reponn nan sit sa a.",
+    "homeless": "Se sit sa a gen entansyon pou moun ki fè eksperyans san kay.",
+    "telemed": "Yon vizit telemedisin obligatwa anvan fè tès nan sit sa a.",
+    "onlineQuest": "Yon kesyonè sou entènèt dwe ranpli anvan ou vizite sit sa a."
+  },
+  "notes": {
+    "schedApp": "Randevou yo mande pou tès.",
+    "refReq": "Referans obligatwa.",
+    "schedAppRefReq": "Randevou ak referans obligatwa.",
+    "noApp": "Pa gen randevou ki nesesè pou tès.",
+    "testAll": "Tès bay menm si sentòm yo pa prezan."
+  },
+  "facilityType": {
+    "drugstore": "Dwòg",
+    "fieldSite": "Sit jaden",
+    "clinic": "Klinik",
+    "hospital": "Lopital",
+    "other": "Lòt",
+    "phmcHC": "Sant sante PHMC",
+    "urgentCare": "Swen ijan",
+    "cityHC": "Sant sante vil",
+    "homelessServices": "Sèvis ki san kay"
+  },
+  "tooltips": {
+    "symptomatic": "Pou plis enfòmasyon, gade Sentòm coronavirus (CDC)"
+  }
+}
